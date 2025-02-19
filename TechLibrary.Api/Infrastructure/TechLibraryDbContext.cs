82e8cea06c82c4c6c3f1b3fb8bd91cbb0b24b48b
eduardxdc <eduardxdc@proton.me>
2025-02-19 18:13:29 -0300

    feat: creating and adding the database to the project

---
+++ TechLibrary.Api/Infrastructure/TechLibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.infrastructure
+{
+    public class TechLibraryDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\dev\\TechLibrary\\TechLibraryDb.db");
+        }
+    }
+}
