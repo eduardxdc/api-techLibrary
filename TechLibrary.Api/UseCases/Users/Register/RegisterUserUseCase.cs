1115f32cf8b848caff346117a5fe385b3778d611
eduardxdc <eduardxdc@proton.me>
2025-02-19 18:15:37 -0300

    build: building and testing user endpoints with the database

--- TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq.Expressions;
+﻿using System.Xml.Linq;
+using TechLibrary.Api.Domain.Entities;
+using TechLibrary.Api.infrastructure;
 using TechLibrary.Communication.Requests;
 using TechLibrary.Communication.Responses;
 using TechLibrary.Exception;
@@ -11,8 +13,23 @@ namespace TechLibrary.Api.UseCases.Users.Register
         {
             Validate(request);
 
+            var entity = new User
+            {
+                Email = request.Email,
+                Name = request.Name,
+                Password = request.Password,
+            };
+
+            var dbContext = new TechLibraryDbContext();
+
+            dbContext.Users.Add(entity);
+            dbContext.SaveChanges();
+
             return new ResponseRegisteredUserJson
-            { };
+            {
+                Name = entity.Name,
+
+            };
         }
 
         private void Validate(RequestUserJson request)
